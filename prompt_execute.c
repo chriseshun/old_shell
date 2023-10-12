13811ccacbbfdb446629585ba7476b876cd97bda
chriseshun <eshunchris12@gmail.com>
2023-10-12 16:50:20 -0700

    working on the shell project

--- prompt_execute.c
+++ prompt_execute.c
@@ -6,4 +6,17 @@ void execute_prompt(const char *comd)
 
 	if (c_pid == -1)
 	{ 
-		perror("fork")
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (c_pid == 0) 
+	{
+		execlp(command, command, (char *)NULL);
+		perror("execlp");
+		exit(EXIT_FAILURE);
+	}
+	else 
+	{
+		wait(NULL);
+	}
+}
