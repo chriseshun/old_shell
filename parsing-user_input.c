a676da82ecbb8369c10b894a8f279bcd270857ca
chriseshun <eshunchris12@gmail.com>
2023-10-19 09:07:17 -0700

    fixed the betty issues

--- parsing-user_input.c
+++ parsing-user_input.c
@@ -8,18 +8,18 @@
 
 void read_command(char *comd, size_t size)
 {
-	if (fgets(comd, size, stdin) == NULL)
-	{
-		if (feof(stdin))
-		{
-			 ac_print("/n");
-			 exit(EXIT_SUCCESS);
-		}
-		else
-		{
-			 ac_print("Error while reading input.\n");
-			 exit(EXIT_FAILURE);
-		}
-	}
-	comd[strcspn(comd, "\n")] = '\0';
+if (fgets(comd, size, stdin) == NULL)
+{
+if (feof(stdin))
+{
+ac_print("/n");
+exit(EXIT_SUCCESS);
+}
+else
+{
+ac_print("Error while reading input.\n");
+exit(EXIT_FAILURE);
+}
+}
+comd[strcspn(comd, "\n")] = '\0';
 }
